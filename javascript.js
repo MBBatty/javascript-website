56a18c89512caa6b81c60c33eb4fb0c7279afd6d
unknown <maureenoa@hotmail.com>
2023-11-15 16:06:06 -0500

    post 11-14-23 classwork

--- javascript.js
+++ javascript.js
@@ -1,4 +1,14 @@
-function onClick(inputName) {
-    document.getElementById("output").innerHTML = "Now I am in control";
-    alert('Hello, ' + inputName + '!');
+function onClick(inputValue) {
+    document.getElementById("displayedText").innerHTML = "Now I am in control";
+    const user = {name: 'Bob'};
+    console.log('Hello ' + user.name);
+
+    const names = ['Sally', 'Jerome']
+    const users = [{name: 'Sally'}, {name: 'Jerome'}]
+    console.log ('First name in list -----',names[0]);
+
+    console.log ('Second name in list of objects -----',users[1].name);
+    alert('Hello, ' + inputValue + '!');
+
+    $.get(URL,callback);
 }
