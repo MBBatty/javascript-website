1484096128e20ee03a47f70fd9ab111ceb8ecddf
unknown <maureenoa@hotmail.com>
2023-11-21 13:26:54 -0500

    trying again hopefully won't lose this

--- javascript.js
+++ javascript.js
@@ -1,14 +1,22 @@
 function onClick(inputValue) {
     document.getElementById("displayedText").innerHTML = "Now I am in control";
     const user = {name: 'Bob'};
-    console.log('Hello ' + user.name);
+    console.log('Hello ' + user.name);hkmv
 
     const names = ['Sally', 'Jerome']
     const users = [{name: 'Sally'}, {name: 'Jerome'}]
     console.log ('First name in list -----',names[0]);
 
     console.log ('Second name in list of objects -----',users[1].name);
-    alert('Hello, ' + inputValue + '!');
+   const url = 'https://pokeapi.co/api/v2/pokemon';
+    $.get(url,function(data, status){
+        if (status === 'success') {
+            console.log(data.results.map(showNames));
+        }
+    });
+     alert('Hello, ' + inputValue + '!');
+}
 
-    $.get(URL,callback);
+function showNames(pokemon) {
+    return pokemon.name
 }
