75bc424a6255e4575862f36a215fcdf9b7fa6433
unknown <maureenoa@hotmail.com>
2023-11-29 12:02:53 -0500

    added jquery and made clal to get data from pokemon api

--- javascript.js
+++ javascript.js
@@ -11,10 +11,13 @@ function onClick(inputValue) {
    const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
-            console.log(data.results.map(showNames));
+            const pokemonNames = data.results.map(showNames)
+            console.log(pokemonNames);
+            document.getElementById("displayedText").innerHTML = pokemonNames.join(' ');
         }
     });
      alert('Hello, ' + inputValue + '!');
+    
 }
 
 function showNames(pokemon) {
