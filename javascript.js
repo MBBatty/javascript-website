1ee1fe9b6aa514e7fa2ef82b58ed3b477529a803
unknown <maureenoa@hotmail.com>
2023-11-30 09:47:50 -0500

    remove old code

--- javascript.js
+++ javascript.js
@@ -1,13 +1,5 @@
-function onClick(inputValue) {
-    document.getElementById("displayedText").innerHTML = "Now I am in control";
-    const user = {name: 'Bob'};
-    console.log('Hello ' + user.name);
+function onClick(inputName) {
 
-    const names = ['Sally', 'Jerome']
-    const users = [{name: 'Sally'}, {name: 'Jerome'}]
-    console.log ('First name in list -----',names[0]);
-
-    console.log ('Second name in list of objects -----',users[1].name);
    const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
@@ -16,8 +8,7 @@ function onClick(inputValue) {
             document.getElementById("displayedText").innerHTML = pokemonNames.join(' ');
         }
     });
-     alert('Hello, ' + inputValue + '!');
-    
+        
 }
 
 function showNames(pokemon) {
