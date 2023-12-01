d8f5c83a1a0ea1238d5424ddb631185912647e94
unknown <maureenoa@hotmail.com>
2023-12-01 04:21:12 -0500

    transformed list into list of h2 tags with map

--- javascript.js
+++ javascript.js
@@ -3,7 +3,9 @@ function onPress(inputName) {
    const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
-            const pokemonNames = data.results.map(showNames)
+            const pokemonNames = data.results.map(pokemon => {
+                return '<h2>' + pokemon.name + '</h2>';
+            });
             console.log(pokemonNames);
             document.getElementById("displayedText").innerHTML = pokemonNames;
         }
@@ -11,6 +13,3 @@ function onPress(inputName) {
         
 }
 
-function showNames(pokemon) {
-    return JSON.stringify(pokemon);
-}
