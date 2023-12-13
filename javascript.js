7910cbf446cb1ffa81493e9af0252bacce3ed9e8
unknown <maureenoa@hotmail.com>
2023-12-12 20:05:14 -0500

    after 12/12/23 class

--- javascript.js
+++ javascript.js
@@ -1,15 +1,18 @@
 function onPress(inputName) {
-
    const url = 'https://pokeapi.co/api/v2/pokemon';
     $.get(url,function(data, status){
         if (status === 'success') {
             const pokemonNames = data.results.map(pokemon => {
-                return '<h2>' + pokemon.name + '</h2>';
+               const h2Tag = document.createElement('h2');
+               h2Tag.
+               return '<h2>' + pokemon.name + '</h2>';
             });
             console.log(pokemonNames);
             document.getElementById("displayedText").innerHTML = pokemonNames;
+        } else {
+            console.log('Status was: ', status)
         }
-    });
+    }).then();
         
 }
 
